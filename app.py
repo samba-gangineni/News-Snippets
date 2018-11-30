818ee5afa01870e34b637e16fff6070efdcb86c3
samba693 <sambasivarao693@gmail.com>
2018-11-30 13:49:52 -0500

    Starter files

---
+++ app.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from newsapi import NewsApiClient
+from flask import Flask, render_template
+
+__author__="Sambasiva Rao Gangineni"
+
+#Creating the Flask application
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+@app.route('/home',methods=['GET','POST'])
+def home():
+    return render_template('index.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ script.py
@@ -0,0 +1,3 @@
+'''
+    Get the data from the google news api for testing the top modelling
+'''
