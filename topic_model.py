857c78af9fc0b8c9a5190e86b7ce069723140f6b
samba693 <sambasivarao693@gmail.com>
2018-12-01 14:04:05 -0500

    Reading in data

--- topic_model.py
+++ topic_model.py
@@ -2,16 +2,38 @@
     Get the data from the google news api for testing the top modelling
 '''
 from __future__ import print_function
+from nltk.tokenize import RegexpTokenizer
 import json
 
 
 __author__="Sambasiva Rao Gangineni"
 
+'''
+    Preprocessing the data
+'''
 #Reading in the newsarticles
-news_articles = []
+news_content = []
+url = []
+title=[]
+dop=[]
+count = 0
 with open('news-reuters-2014.json','r') as news:
     for article in news:
-        news_articles.append(article)
+        try:
+            test = json.loads(article)
+            urls = test.get('url')
+            dops = test.get('dop')
+            texts = test.get('text')
+            titles = test.get('title')
+            if '(Reuters)' in texts and '(Reporting by ' in texts:
+                texts = texts[texts.index('(Reuters)')+9:texts.index('(Reporting by ')]
+            news_content.append(texts)
+            url.append(urls)
+            title.append(titles)
+            dop.append(dops)
+        except:
+            count+=1
+
+# Tokenising the words
 
 
-print(len(news_articles))
