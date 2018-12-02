cf076ac1a7ec02110f5066d64d171510045685e8
samba693 <sambasivarao693@gmail.com>
2018-12-02 11:59:24 -0500

    Doing bigram and lemmatisation

--- topic_model.py
+++ topic_model.py
@@ -7,8 +7,11 @@ from stopwords import get_stopwords
 from datetime import datetime
 from gensim import models,corpora
 from copy import deepcopy
+from pprint import pprint
 import json
 import sys
+import matplotlib.pyplot as plt
+import spacy
 import pyLDAvis
 import pyLDAvis.gensim
 import operator
@@ -22,19 +25,25 @@ def optimum_topics(corpus,list_topics,dictionary,iterations,processed_content):
     temp_model = None
     topics = 0
     model_coherences = []
+    model_preplexities = []
+    models_tosave = []
     for i in range(0,len(list_topics)):
         lda_model = models.LdaModel(corpus=deepcopy(corpus),num_topics=list_topics[i],id2word=deepcopy(dictionary),iterations=iterations)
-
+        models_tosave.append(lda_model)
+        #model perplexity calculation
+        model_preplexity = lda_model.log_perplexity(corpus)
+        model_preplexities.append(model_preplexity)
         #Calculating the coherence
         topic_coherence = models.CoherenceModel(model=lda_model, texts=processed_content, dictionary=dictionary, coherence='c_v')
         model_coherence = topic_coherence.get_coherence()
         model_coherences.append(model_coherence)
+
         if model_coherence>=temp:
             topics = list_topics[i]
             temp = model_coherence
             temp_model = lda_model
     
-    return temp_model,topics,temp,model_coherences
+    return temp_model,topics,temp,model_coherences, model_preplexities, models_tosave
 
 __author__="Sambasiva Rao Gangineni"
 
@@ -42,16 +51,16 @@ __author__="Sambasiva Rao Gangineni"
     Preprocessing the data
 '''
 
-#Reading in the 50000 newsarticles
+#Reading in the 20000 newsarticles
 url = []
 title = []
 dop = []
 content = []
 count =  0
-ten_thousand = 0
+twenty_thousand = 0
 with open(sys.argv[1],'r') as news:
     for article in news:
-        if ten_thousand<=10000:
+        if twenty_thousand<=20000:
             try:
                 # Loadings the json object
                 test = json.loads(article)
@@ -73,16 +82,16 @@ with open(sys.argv[1],'r') as news:
                 content.append(texts)
 
                 #Condition for reading in the 50000 articles
-                ten_thousand+=1
+                twenty_thousand+=1
 
             except:
                 count+=1
 
-#Tokenising, stopping and stemming the content
+#Tokenising
 tokenizer = RegexpTokenizer(r'[a-zA-Z]{3,}')
 english_stopwords = get_stopwords('en')
 english_stopwords.append('reutuers')
-
+token_content = []
 processed_content = []
 for article in content:
     tokens = tokenizer.tokenize(article.lower())
@@ -95,9 +104,23 @@ for article in content:
             tokens.remove('said')
         if 't' in tokens:
             tokens.remove('t')
-    
+    token_content.append(tokens)
     stopped_tokens = [i for i in tokens if i not in english_stopwords]
     processed_content.append(stopped_tokens)
+
+
+# Creating a bigram model
+bigram = models.Phrases(token_content, min_count=5, threshold = 100)
+bigram_mod = models.phrases.Phraser(bigram)
+bigram_content = [bigram_mod[i] for i in processed_content]
+
+# Lemmatising using spacy
+nlp = spacy.load('en',disable=['parser','ner'])
+allowed_tags = ['NOUN','ADJ','VERB','ADV']
+lemmatised_content = []
+for i in bigram_content:
+    docu = nlp(" ".join(i))
+    lemmatised_content.append([j.lemma_ for j in docu if j.pos_ in allowed_tags])
 
 '''
     Topic modelling using LDA
@@ -110,41 +133,28 @@ corpus = [dictionary.doc2bow(text) for text in processed_content]
 
 #Building the LDA model with optimum topics
 topics_list = range(2,12)
-lda_model, num_topics, model_coherence, list_coherences = optimum_topics(corpus,topics_list,dictionary,150,processed_content)
+lda_model, num_topics, model_coherence, list_coherences, list_preplexity, models_tosave = optimum_topics(corpus,topics_list,dictionary,350,processed_content)
 
 '''
-    Model Evaluation
+    Evaluating the model
 '''
-#model perplexity calculation
-model_preplexity = lda_model.log_perplexity(corpus)
 
-'''
-    Visualising the model
-'''
-visualisation = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary)
+# Line graph for model_coherence to select the optimal number of topics
+x = range(2,12,1)
+plt.plot(x,list_coherences)
+plt.xlabel("Num of topics")
+plt.ylabel("Coherence score")
+plt.legend(("Coherence values"),loc = 'best')
+plt.savefig('coherence.png')
+plt.clf()
 
-# Saving the visualisation to the html
-pyLDAvis.save_html(visualisation,'topic_model.html')
-
-# Labelling the document using the model
-labels = []
-for i in content:
-    tokens = tokenizer.tokenize(i.lower())
-    while 'reuters' in tokens or 's' in tokens or 'said' in tokens or 't' in tokens:
-        if 'reuters' in tokens:
-            tokens.remove('reuters')
-        if 's' in tokens:
-            tokens.remove('s')
-        if 'said' in tokens:
-            tokens.remove('said')
-        if 't' in tokens:
-            tokens.remove('t')
-    
-    stopped_tokens = [j for j in tokens if j not in english_stopwords]
-    bow = dictionary.doc2bow(stopped_tokens)
-    prediction = lda_model[bow]
-    best_label = max(dict(prediction).iteritems(), key = operator.itemgetter(1))[0]
-    labels.append(best_label)
+# Line graph for model preplexity to select the optimal number of topics
+plt.plot(x,list_preplexity)
+plt.xlabel("Num of topics")
+plt.ylabel("preplexity")
+plt.legend(("preplexity values"),loc = 'best')
+plt.savefig('preplexity.png')
+plt.clf()
 
 '''
     Saving the important list for summarisation
@@ -165,13 +175,14 @@ with open('dop.pkl','w') as f:
 with open('content.pkl','w') as f:
     pickle.dump(content,f)
 
-# labels
-with open('labels.pkl','w') as f:
-    pickle.dump(labels,f)
+#Saving the preplexity list for plotting later
+with open('preplexity.pkl','w') as f:
+    pickle.dump(list_preplexity,f)
 
 #Saving the coherence list for plotting later
 with open('coherence.pkl','w') as f:
     pickle.dump(list_coherences,f)
 
 # saving the topics
-lda_model.save('lda.model')
+for i in range(0,len(models_tosave)):
+    models_tosave[i].save('lda{}.model'.format(i))
