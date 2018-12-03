23e00f42f617121e6071ceb6e5523f4052f79c64
samba693 <sambasivarao693@gmail.com>
2018-12-02 20:25:16 -0500

    Summarising the text yet to test

--- text_summarizer.py
+++ text_summarizer.py
@@ -1,11 +1,15 @@
 from __future__ import print_function
 from __future__ import unicode_literals
+from stopwords import get_stopwords
 from nltk.corpus import stopwords
 from nltk.cluster.util import cosine_distance
+from operator import itemgetter
 import numpy as np
+import pickle
+
+np.seterr(divide='ignore', invalid='ignore')
 
 __author__="Sambasiva Rao"
-
 ''' 
     Summarisation using the Text Rank 
     - Text rank algorithm 
@@ -19,7 +23,6 @@ def text_rank(similarity_matrix,precision,damping_factor):
     
     # Initializing the rank vector
     rank_vector = np.ones(vector_length)/vector_length
-
     # Algorithm
     while True:
         new_ranks = (np.ones(vector_length)*(1-damping_factor)/vector_length)+damping_factor*(similarity_matrix.T.dot(rank_vector))
@@ -62,7 +65,7 @@ def sentence_similarity(sent1,sent2,stopwords=None):
     return 1-cosine_distance(vector1,vector2)
 
 # constructing a sentence similarity matrix
-def build_similarity_matrix(sentences, stopwords=None):
+def similarity_matrix(sentences, stopwords=None):
     
     # number of sentences
     num_sentences = len(sentences)
@@ -80,7 +83,63 @@ def build_similarity_matrix(sentences, stopwords=None):
 
     # Normalising the matrix
     for i in range(num_sentences):
-        similarity_matrix[i]/=similarity_matrix[i].sum()
+        if similarity_matrix[i].sum()!=0:
+            similarity_matrix[i]=similarity_matrix[i]/similarity_matrix[i].sum()
     
     return similarity_matrix
 
+# summarising and getting top sentences
+def summarize_article(sentences,num_of_sentences, stopwords = None):
+
+    # Calculating the similarity matrix for the article
+    s_matrix = similarity_matrix(sentences, stopwords)
+
+    # Generating the rank vector
+    sentence_ranks = text_rank(s_matrix,0.0001,0.85)
+
+    # Sorting the sentence ranks and taking the required number of sentences
+    ranked_sentence_indexes = [item[0] for item in sorted(enumerate(sentence_ranks), key=lambda item: -item[1])]
+    selected_sentences = sorted(ranked_sentence_indexes[:num_of_sentences])
+    summary = itemgetter(*selected_sentences)(sentences)
+    return summary
+
+# Loading the news content
+with open('content.pkl','r') as f:
+    content = pickle.load(f)
+
+# stop words
+english_stopwords = get_stopwords('en')
+english_stopwords.append('-')
+english_stopwords.append('*')
+english_stopwords.append('_')
+
+# Summarising all the articles
+summarized_content = []
+for article in content:
+    article = article.split('.')
+    while '' in article or '*' in article or '-' in article:
+        if '' in article:
+            article.remove('')
+        if '*' in article:
+            article.remove('*')
+        if '-' in article:
+            article.remove('-')
+    
+    sentences = []
+    for sentence in article:
+        se.append(sentence.split())
+    
+    #summarising
+    result = summarize_article(sentences,1,english_stopwords)
+
+    # making the result into a sentence
+    summary = " ".join(result[0])
+    print(summary)
+    # Appending the result to a list
+    summarized_content.append(summary)
+
+'''
+    Saving the summarised content
+'''
+with open('summary.pkl','w') as f:
+    pickle.dump(summarized_content,f)
